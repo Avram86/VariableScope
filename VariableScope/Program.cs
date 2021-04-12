f1d6ea1c8d9e4b2a91add43e2295e0f464284e8f
Avram86 <60349668+Avram86@users.noreply.github.com>
2021-04-12 19:46:25 +0300

    Add project files.

---
+++ VariableScope/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace VariableScope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlockScope();
+        }
+        private static void BlockScope()
+        {
+            {
+                //here is a block scope parent
+                int i = 10;
+                i = i + 30;
+                Console.WriteLine(i);
+                {
+                    //here is a child block scope
+
+                    //not allowed
+                    //int i=5;
+
+                    //we can reuse variables defined in the parent block scope
+                    i = i * 5;
+                    Console.WriteLine(i);
+                }
+            }
+
+            //there are specific C# works that open block scopes
+            //e.g.: if, for,///
+            if (true)
+            {
+               // .....
+            }
+        } 
+    }
+}
